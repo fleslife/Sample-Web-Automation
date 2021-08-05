b59dae95909e652460bbd7251cdfdbe9662201bd
LAPTOP-03R2ANRI\admin <flespj@gmail.com>
2021-08-05 12:25:07 +0700

    Update FJR 05082021-1225

---
+++ Test Suites/Regression.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Regression</description>
+   <name>Regression</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient>flespj@gmail.com;</mailRecipient>
+   <numberOfRerun>1</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>true</rerunFailedTestCasesOnly>
+   <testSuiteGuid>69534e80-68e3-4ebb-aecb-7e365fb41ab0</testSuiteGuid>
+   <testCaseLink>
+      <guid>70d8ed4a-b7b2-4723-9178-66c0c6a30f77</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Practice Form</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
