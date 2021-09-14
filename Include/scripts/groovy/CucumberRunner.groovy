1e85f28fa5c44f1599cd56028e8eae1248f43c74
LAPTOP-03R2ANRI\admin <flespj@gmail.com>
2021-09-14 10:28:34 +0700

    Disable CucumberRunner

--- Include/scripts/groovy/CucumberRunner.groovy
+++ Include/scripts/groovy/CucumberRunner.groovy
@@ -1,3 +1,4 @@
+/*
 import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
 import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
 import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
